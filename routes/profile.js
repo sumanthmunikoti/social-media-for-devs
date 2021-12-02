f4c1f37c7701e3dbbc121b06e7a74968f28a688d
sumanthmunikoti <munikoti.s@northeastern.edu>
2021-12-02 18:51:02 -0500

    Server up and running

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,3 @@
-const { request } = require('express')
 const express = require('express')
 const router = express.Router()
 const Profile = require('../models/userProfile')
@@ -8,6 +7,18 @@ router.get('/me', async (req, res) => {
         const profile = await Profile.findOne({
             user: req.user.id
         })
-        .populate('user', ['name'])
+        .populate('user')
+
+        //check to see if there's no profile
+        if (!profile) {
+            return response.status(400).json({msg: 'Profile for this user does not exist!'})
+        }
+        //if there is a profile
+        await response.json(profile);
+    } catch (e) {
+        console.error(e.message);
+        response.status(500).send('Server Error');
     }
 })
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const profile = require('./routes/profile')
+// const mongoDB = process.env.MONGODB_URI;
+const mongoDB = "mongodb+srv://root:rootroot@codeb-1za4v.mongodb.net/codebook?retryWrites=true&w=majority"
 
 const app = express();
-
 const port = process.env.PORT || 3002;
 
+app.use(cors())
+app.options('*', cors());
+
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.json({extended:false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('socmed/profile', profile)
 
 app.listen(port, () => {
