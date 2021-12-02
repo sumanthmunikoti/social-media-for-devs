ba17e95d82b1a05b43f8e98c0ebe49b8b05c04a7
sumanthmunikoti <munikoti.s@northeastern.edu>
2021-12-02 14:57:01 -0500

    Server setup

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 3002;
 
+app.use('socmed/profile', profile)
+
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });
