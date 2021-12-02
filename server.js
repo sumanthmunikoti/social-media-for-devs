f838d94efb94a77593220aee91b4f1c4f943fb48
sumanthmunikoti <munikoti.s@northeastern.edu>
2021-12-02 12:49:09 -0500

    test commit

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
