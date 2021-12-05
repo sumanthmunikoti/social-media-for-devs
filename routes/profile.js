d3816609ffbb7103f540f80915fd51b66650abf5
sumanthmunikoti <munikoti.s@northeastern.edu>
2021-12-05 12:21:11 -0500

    Phone number saved in profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,19 +5,34 @@ const mongoose = require('mongoose')
 
 router.post('/me', async (req, res) => {
 
-    console.log(req.body)
-
     try {
         const profile = await Profile.find({
             user: mongoose.Types.ObjectId(req.body.userId)
         })
 
-        console.log('profile', profile)
         res.json(profile);
 
     } catch (e) {
         console.error(e.message);
         response.status(500).send('Server Error');
+    }
+})
+
+router.post('/phone', async (req, res) => {
+
+    const phoneNumber = req.body.phone;
+
+    try {
+        const profile = await Profile.findOne({
+            user: mongoose.Types.ObjectId(req.body.userId)
+        })
+
+        profile.phone = phoneNumber;
+        await profile.save()
+        res.json(profile)
+    } catch (e) {
+        console.error(e.message)
+        res.status(500).send('Server error')
     }
 })
 
