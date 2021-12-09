3fb61d08aa835dc8e1911f975140bd4ab1e031b8
sumanthmunikoti <munikoti.s@northeastern.edu>
2021-12-09 17:32:16 -0500

    Saving changes

--- routes/posts.js
+++ routes/posts.js
@@ -14,7 +14,6 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    console.log("............")
     try {
         const user = await User.findById(req.body.userId)
 
@@ -30,6 +29,26 @@ router.post('/', async (req, res) => {
         return res.status(500).json('server error');
     }
 
+})
+
+
+router.put('/like/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        const filteredPost = post.likes.filter(like => like.user.toString() === req.body.userId);
+
+        if (filteredPost.length > 0) {
+            return res.status(400).json('You have already liked the post');
+        } else {
+            const like = { user: req.body.userId };
+            post.likes.push(like);
+            await post.save();
+            res.json(post.likes);
+        }
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('Server error');
+    }
 })
 
 module.exports = router
